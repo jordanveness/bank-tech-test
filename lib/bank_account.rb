718c83669e49d3430562b6ebe2ffe1df5645cdee
Jordan <jordanveness8@gmail.com>
2021-10-27 09:39:22 +0100

    refacted code

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,8 +1,16 @@
 class BankAccount
 
-def deposit
-  250
-end
+  attr_reader :balance
 
+  def initialize
+    @balance = 0
+  end
 
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def withdraw(amount)
+    @balance -= amount
+  end
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,8 +1,23 @@
-require './lib/bank_account'
+require 'bank_account'
 
-describe BankAccount do 
-  it "should allow you to deposit a sum of money" do
-    bank_account = BankAccount.new
-    expect(bank_account.deposit).to equal(250)
+describe BankAccount do
+  it 'has a balance of zero' do
+    expect(subject.balance).to eq(0)
+  end
+
+  describe '#deposit' do
+
+    it { is_expected.to respond_to(:deposit).with(1).argument }
+    it 'can deposit a sum of money' do
+      expect{ subject.deposit 1 }.to change{ subject.balance }.by 1
+    end
+  end
+
+  describe '#withdraw' do
+
+    it { is_expected.to respond_to(:withdraw).with(1).argument }
+    it 'can withdraw a sum of money' do
+      expect{ subject.withdraw 1 }.to change{ subject.balance }.by -1
+    end
   end
 end
